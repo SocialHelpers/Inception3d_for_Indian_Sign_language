dee21078494cd21818d20db57e5e2697ab2b8ef5
Rochak Agrawal <24rochak@gmail.com>
2019-03-18 14:44:37 +0530

    Changed Variable names.

--- Class_sort.py
+++ Class_sort.py
@@ -1,45 +1,35 @@
 import os
 import shutil
-import numpy as np
-import pandas as pd
-import warnings
 from imutils import paths
-import cv2
 
 def create_folders(sPath):
-    li_videos=[]
     cnt = 1
     os.mkdir("Classes")
-    ClassPath = sPath + "\Classes"
+    ClassPath = os.path.join(sPath,"Classes")
     os.chdir(ClassPath)
-    sVideoPath=sPath+"\Augmented_FinalDataset"
-    li_videos=os.listdir(sVideoPath)
-    for item in range(len(li_videos)*5):
+    for item in range(105):
         ClassName=str(cnt).zfill(4)
-        ClassFolderPath=ClassPath+"\\"+ClassName
+        ClassFolderPath=os.path.join(ClassPath,ClassName)
+        print("Creating folder : ",ClassFolderPath)
         os.mkdir(ClassFolderPath)
         cnt+=1
     os.chdir(sPath)
+
 def folders2classes(sFolderPath):
-    print(sFolderPath)
-    sClassPath = sFolderPath + "\\"+"Classes"
-    os.chdir(sClassPath)
+    sClassPath = os.path.join(sFolderPath,"Classes")
     pi_videos=os.listdir(sClassPath)
-    os.chdir(sFolderPath)
     li_videos=paths.list_files("Augmented_FinalDataset")
     cnt=0
     for item in li_videos:
         cnt += 1
         id = item.split(".")[0]
-        pid=id[-6:-2]
-        #print(item)
-        #print(pid)
-        if pid in pi_videos:
-            sVideoName = str(cnt).zfill(4)
-            sClassSavePath=sClassPath+"\\"+pid+"\\"+sVideoName+".mp4"
-            #print(sClassSavePath)
+        class_id=id[-6:-2]
+        if class_id in pi_videos:
+            sVideoName = str(cnt).zfill(4)+".mp4"
+            sClassSavePath=os.path.join(sClassPath,os.path.join(class_id,sVideoName))
+            print("Adding video to : ",sClassSavePath)
             shutil.copy(src=item,dst=sClassSavePath)
 
-str1 = os.getcwd()
-create_folders(str1)
-folders2classes(str1)
+cwd = os.getcwd()
+create_folders(cwd)
+folders2classes(cwd)
