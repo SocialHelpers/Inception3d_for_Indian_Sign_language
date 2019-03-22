cfb3c29cae493eb85694bfd994231503777de860
Rochak Agrawal <24rochak@gmail.com>
2019-03-23 00:54:30 +0530

    Update resize.py

--- resize.py
+++ resize.py
@@ -1,47 +1,56 @@
+"""
+This script provides functionality to resize videos to (320,240).
+It also reduces the frame rate to 10 fps.
+It uses "ffmpeg" for the above stated purpose.
+"""
+
 import os
 
-#print("\nAnalyze video durations and fps from %s ..." % (sVideoDir))
-sVideoDir=os.path.join(os.getcwd(),"Dataset")
+print("\n Resizing and Analyzing fps from %s ..." % (sVideoDir))
+
+sVideoDir = os.path.join(os.getcwd(), "Dataset")
 print(sVideoDir)
 liVideos = []
-pathlist=os.listdir(sVideoDir)
-Folders=[]
-for item in pathlist:
-	inner = os.path.join(sVideoDir,item)
-	innerpaths=os.listdir(inner)
-	Folders.append(inner)
-	for item2 in innerpaths:
-		inner2=os.path.join(inner,item2)
-		liVideos.append(inner2)
+pathlist = os.listdir(sVideoDir)
+Folders = []
 
-cmd="ffmpeg -y -i {} -r 25 -s 320x240 -r 10 {}"
+for item in pathlist:
+    inner = os.path.join(sVideoDir, item)
+    innerpaths = os.listdir(inner)
+    Folders.append(inner)
+    for item2 in innerpaths:
+        inner2 = os.path.join(inner, item2)
+        liVideos.append(inner2)
+
+cmd = "ffmpeg -y -i {} -r 25 -s 320x240 -r 10 {}"
 
 os.mkdir("FinalDataset")
-cwd=os.getcwd()
+cwd = os.getcwd()
 os.chdir("FinalDataset")
 
-f=sorted(set(Folders))
-folders=[]
+f = sorted(set(Folders))
+folders = []
+
 for item in f:
-	a=item.split("\\")
-	#print(a[-1])
-	os.mkdir(a[-1])
-	ce=os.getcwd()
-	ce1=os.path.join(ce,a[-1])
-	folders.append(ce1)
+    a = item.split("\\")
+    os.mkdir(a[-1])
+    ce = os.getcwd()
+    ce1 = os.path.join(ce, a[-1])
+    folders.append(ce1)
+
 os.chdir(cwd)
-if len(liVideos) == 0: raise ValueError("No videos detected")
-j=0
-cnt=1
+j = 0
+cnt = 1
+
 for item in liVideos:
-	source=item
-	id = str(cnt).zfill(4) + ".mp4"
-	dest=folders[j]
-	dest=os.path.join(dest,id)
-	cmd1 = cmd.format(source,dest)
-	print(cmd1)
-	os.system(cmd1)
-	cnt+=1
-	if cnt==106:
-		cnt=1
-		j+=1
+    source = item
+    id = str(cnt).zfill(4) + ".mp4"
+    dest = folders[j]
+    dest = os.path.join(dest, id)
+    formatted_cmd = cmd.format(source, dest)
+    print(formatted_cmd)
+    os.system(formatted_cmd)
+    cnt += 1
+    if cnt == 106:
+        cnt = 1
+        j += 1
