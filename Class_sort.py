301b80a1f0f89fe9ac624866abc616f1b16d0309
Rochak Agrawal <24rochak@gmail.com>
2019-03-23 00:51:39 +0530

    Update Class_sort.py

--- Class_sort.py
+++ Class_sort.py
@@ -5,6 +5,7 @@ import os
 import shutil
 from imutils import paths
 
+
 def create_folders(sPath):
     """
     Creates the required folders
@@ -13,15 +14,16 @@ def create_folders(sPath):
     """
     cnt = 1
     os.mkdir("Classes")
-    ClassPath = os.path.join(sPath,"Classes")
+    ClassPath = os.path.join(sPath, "Classes")
     os.chdir(ClassPath)
     for item in range(105):
-        ClassName=str(cnt).zfill(4)
-        ClassFolderPath=os.path.join(ClassPath,ClassName)
-        print("Creating folder : ",ClassFolderPath)
+        ClassName = str(cnt).zfill(4)
+        ClassFolderPath = os.path.join(ClassPath, ClassName)
+        print("Creating folder : ", ClassFolderPath)
         os.mkdir(ClassFolderPath)
-        cnt+=1
+        cnt += 1
     os.chdir(sPath)
+
 
 def folders2classes(sFolderPath):
     """
@@ -29,19 +31,20 @@ def folders2classes(sFolderPath):
     :param sFolderPath: Base folder where the original and class folders are present.
     :return: None
     """
-    sClassPath = os.path.join(sFolderPath,"Classes")
-    pi_videos=os.listdir(sClassPath)
-    li_videos=paths.list_files("Dataset\Augmented_FinalDataset")
-    cnt=0
+    sClassPath = os.path.join(sFolderPath, "Classes")
+    pi_videos = os.listdir(sClassPath)
+    li_videos = paths.list_files("Dataset\Augmented_FinalDataset")
+    cnt = 0
     for item in li_videos:
         cnt += 1
         id = item.split(".")[0]
-        class_id=id[-6:-2]
+        class_id = id[-6:-2]
         if class_id in pi_videos:
-            sVideoName = str(cnt).zfill(5)+".mp4"
-            sClassSavePath=os.path.join(sClassPath,os.path.join(class_id,sVideoName))
-            print("Adding video to : ",sClassSavePath)
-            shutil.copy(src=item,dst=sClassSavePath)
+            sVideoName = str(cnt).zfill(5) + ".mp4"
+            sClassSavePath = os.path.join(sClassPath, os.path.join(class_id, sVideoName))
+            print("Adding video to : ", sClassSavePath)
+            shutil.copy(src=item, dst=sClassSavePath)
+
 
 cwd = os.getcwd()
 create_folders(cwd)
